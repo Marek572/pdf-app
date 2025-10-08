1a7a80e88d8846df70bfee37bea5d66fd9703e5a
marek572 <marek.bp@poczta.fm>
2025-10-08 12:35:48 +0200

    PA-19 adding PUT request and removeFieldsValues func

--- be/src/app.ts
+++ be/src/app.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import multer from 'multer';
-import { getPdfFields } from './pdf-service';
+import { getPdfFields, removeFieldsValues } from './pdf-service';
 import fs from 'fs';
 import path from 'path';
 
 const app = express();
-const port = 3000;
+const port: number = 3000;
 
 app.use(cors());
 
@@ -31,7 +31,7 @@ const storage = multer.diskStorage({
     cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
+    cb(null, file.originalname);
   },
 });
 
@@ -42,16 +42,39 @@ app.post('/pdf/upload', upload.single('file'), async (req: Request, res: Respons
     if (!req.file) {
       return res.status(400).json({ message: 'No file uploaded' });
     }
-    // Read the file from disk to get the buffer
-    const fileBuffer = fs.readFileSync(req.file.path);
-    const fields = await getPdfFields(fileBuffer);
-    res.json({
-      message: 'File uploaded successfully',
-      fields,
-    });
+
+    return res.status(200).end();
   } catch (err) {
     console.error(err);
     res.status(500).send('Failed to parse PDF');
+  }
+});
+
+app.put('/pdf/:fileName', async (req: Request, res: Response) => {
+  const { fileName } = req.params;
+
+  try {
+    if (!fileName) {
+      return res.status(400).json({ message: 'No filename provided' });
+    }
+    const filePath: string = path.join(uploadDir, fileName);
+    const fileBuffer: Buffer = fs.readFileSync(filePath);
+    const fields = await getPdfFields(fileBuffer);
+
+    if (fields.length === 0) {
+      return res.status(400).json({ error: { message: 'No form fields found in the PDF' } });
+    }
+
+    const modifiedBuffer = await removeFieldsValues(fileBuffer);
+
+    fs.writeFileSync(filePath, modifiedBuffer);
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', `attachment; filename=${fileName}`);
+    res.send(Buffer.from(modifiedBuffer));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Failed to remove fields values');
   }
 });
 
--- be/src/pdf-service.ts
+++ be/src/pdf-service.ts
@@ -1,5 +1,13 @@
-import fs from "fs";
-import { PDFDocument } from "pdf-lib";
+import fs from 'fs';
+import {
+  PDFCheckBox,
+  PDFDocument,
+  PDFDropdown,
+  PDFField,
+  PDFOptionList,
+  PDFRadioGroup,
+  PDFTextField,
+} from 'pdf-lib';
 
 export async function getPdfFields(fileBuffer: Buffer) {
   const pdfDoc: PDFDocument = await PDFDocument.load(fileBuffer);
@@ -7,8 +15,31 @@ export async function getPdfFields(fileBuffer: Buffer) {
   const form = pdfDoc.getForm();
   const fields = form.getFields();
 
-  return fields.map((field) => {
-    const type = field.constructor.name;
+  return fields.map((field: PDFField) => {
+    const type: string = field.constructor.name;
     return { name: field.getName(), type };
   });
+}
+
+export async function removeFieldsValues(fileBuffer: Buffer) {
+  const pdfDoc: PDFDocument = await PDFDocument.load(fileBuffer);
+
+  const form = pdfDoc.getForm();
+  form.getFields().forEach((field: PDFField) => {
+    const type: string = field.constructor.name;
+    if (field instanceof PDFTextField) {
+      field.setText('');
+    } else if (field instanceof PDFCheckBox) {
+      field.uncheck();
+    } else if (field instanceof PDFDropdown) {
+      field.clear();
+    } else if (field instanceof PDFRadioGroup) {
+      field.clear();
+    } else if (field instanceof PDFOptionList) {
+      field.clear();
+    }
+  });
+
+  const pdfBytes = await pdfDoc.save();
+  return pdfBytes;
 }
